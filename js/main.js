deec40d4bb291dc354c79558d2c0a61f833c8549
AmirHossein-Rahimi <ar5902718@gmail.com>
2025-08-16 16:45:41 +0330

    adding skills and changing scrollbar

--- js/main.js
+++ js/main.js
@@ -4,4 +4,23 @@ const container = document.querySelector('.container');
 window.addEventListener('load', () => {
     container.style.opacity = "1";
     container.style.transform = "translate(-50%, -40%) scale(1)";
+
+    const dots = document.querySelectorAll('.dots');
+
+    dots.forEach(element => {
+        const fill_count = element.attributes['fill'].value;
+        let total = 4;
+    
+        for (let i = 0; i < fill_count; i++, total--) {
+            const dot = document.createElement('span');
+            dot.classList.add('dot'); //adding dots (filled)
+            dot.classList.add('filled');
+            element.appendChild(dot);
+        }
+        for(let i = 0; i < total; i++) {
+            const dot = document.createElement('span');
+            dot.classList.add('dot'); //adding empty dots (non-filled)
+            element.appendChild(dot);
+        }
+    });
 });
